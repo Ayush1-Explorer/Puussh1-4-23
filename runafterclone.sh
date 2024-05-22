b28a7ec86a3e6dea788d0c911af3978bb67ea1cc
Ayush Pareek <126587776+Ayush1-Explorer@users.noreply.github.com>
2024-05-22 13:50:28 +0530

    Update runafterclone.sh

--- runafterclone.sh
+++ runafterclone.sh
@@ -1,4 +1,4 @@
 apt install npm nodejs -y
-npm install express bcrypt winston path mongoose mongodb dotenv morgan winston-loki axios -y
+npm install express bcrypt winston path mongoose mongodb dotenv morgan winston-loki axios aws-sdk -y
 sudo apt-get install build-essential -y
 node server.js
